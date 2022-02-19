9dcaf370cc1ac72086ff419c5540f514720ef032
afermanx <alexjferman@gmail.com>
2022-02-19 11:09:32 -0400

    vagas e ledsRGB implementado com base na logica da maquete

--- src/index.js
+++ src/index.js
@@ -1,101 +1,359 @@
 
-//chama a biblioteca do jhony-five, pegando o board a ser trabalhada
-var five = require("johnny-five"),
-  board, photoresistor;
-
-
-  //import .env
-  require('dotenv').config()
-
-  //chama o firebase database nao relacional para o fluxo das vagas
-  var firebase = require("firebase");
+// var five = require("johnny-five"),
+//   board, photoresistor;
 
 
 
- // seta a porta do arduino no meu caso a com5
-  board = new five.Board({port :'COM5'});
+
+// board = new five.Board({ port: 'COM5' });
 
 
-  // setanto as config dos parametros para conectar ao databse
-  
- var firebaseConfig = {
-  apiKey: process.env.FB_APIKEY,
-  authDomain: process.env.FB_AUTHDOMAIN,
-  databaseURL: process.env.FB_DATABSEURL,
-  projectId: process.env.FB_PROJECTID,
-  storageBucket: process.env.FB_STORAGEBUCKET,
- 
+const { Board, Led, Sensor } = require("johnny-five");
+const board = new Board({ port: 'COM5' });
+
+var firebase = require("firebase");
+
+
+
+
+//Firebase iniciando as config.
+var firebaseConfig = {
+  apiKey: "AIzaSyBUJj7zSJW1d2r93EosrWI-dqkI5U4aa4M",
+  authDomain: "dbestacione.firebaseapp.com",
+  databaseURL: "https://dbestacione.firebaseio.com",
+  projectId: "dbestacione",
+  storageBucket: "dbestacione.appspot.com",
+
 };
 
 
-
-
-  // inicia a board com os parametros ja setados para começar a brincadeira
-board.on("ready", function() {
+board.on("ready", () => {
 
   firebase.initializeApp(firebaseConfig);
 
-   // setando os  leds para cada pino e passando para um constante
-   const G = new five.Led(13);
+  const andar1 = new Led(13)
+  const terreo = new Led(12)
+
+  const ledVaga1 = new Led.RGB({
+    pins: {
+      red: 6,
+      green: 5,
+      blue: 3
+    },
+    isledVaga1: false
+  });
+
+  const ledVaga2 = new Led.RGB({
+    pins: {
+      red: 11,
+      green: 10,
+      blue: 9
+    },
+    isledVaga2: false
+  });
+  // Add led to REPL (optional)
+  board.repl.inject({ ledVaga1, ledVaga2 });
+
+  // Turn it on and set the initial color
 
 
+  andar1.on()
+  terreo.on()
+  ledVaga1.color("red");
 
-   firebase.database().ref('VAGA1').set({
-    status:'off'
+
+  vaga1 = new Sensor({
+    pin: "A1",
+    freq: 1000
+  });
+
+  vaga2 = new Sensor({
+    pin: "A2",
+    freq: 1000
+  });
+
+  // vaga1 = new Sensor({
+  //   pin: "A2  ",
+  //   freq: 1000
+  // });
+
+
+  vaga1.on("data", function () {
+
+   
+
+    if (this.value >= 900) {
+
+      //   // vaga1
+      firebase.database().ref('VAGA1').set({
+        id: 1,
+        status: 'on',
+        vagaTime: 'off',
+        time: 1000,
+        direction: 'left'
+      });
+
+
+      ledVaga1.color("red");
      
-    
-    
+
+      console.log('red', this.value)
+    } else {
+
+
+      ledVaga1.color("green");
+      
+      firebase.database().ref('VAGA1').set({
+        id: 1,
+        status: 'off',
+        vagaTime: 'off',
+        time: 1000,
+        direction: 'left'
+      });
+
+
+    }
+
+
+
   });
 
 
-  const rootRef = firebase.database().ref().child('VAGA1');
-  const vaga1Ref= rootRef.child('status');
 
-  
 
-  vaga1Ref.on('value', snap =>{
-    // this.setState({
-    //   v1: snap.val()
-      
-    // });
+  // VAGA 2
+  vaga2.on("data", function () {
 
-    snap.val() === "on" ? G.on() : G.off()
-  
-    
    
-    });
+
+    if (this.value >= 900) {
+
+      //   // vaga1
+      firebase.database().ref('VAGA2').set({
+        id: 2,
+        status: 'on',
+        vagaTime: 'off',
+        time: 1000,
+        direction: 'right'
+      });
 
 
+      ledVaga2.color("red");
+     
 
-//   vaga1 = new five.Sensor({
-//     pin: "A0",
-//     freq: 250
-//   });
+      console.log('red', this.value)
+    } else {
 
 
-//   vaga1.on("data", function() {
-//     console.log(this.value)
-//     if(this.value < 1023 ){
+      ledVaga2.color("green");
       
-    
-//       G.off()
-    
-//       // firebase.database().ref('VAGA1').set({
-//       //   VAGA1:'on'
-        
-//       // });
+      firebase.database().ref('VAGA2').set({
+        id: 2,
+        status: 'off',
+        vagaTime: 'off',
+        time: 1000,
+        direction: 'right'
+      });
 
-//     }else if(this.value >= 1023){     
-//       G.on()
-//       // firebase.database().ref('VAGA1').set({
-//       //   VAGA1:'off'
-//       // });
-      
-    
-//     }
-    
 
-//   });
+    }
+
+
+
+  });
 
 });
-console.log("Projeto EstacionaE Iniciado!");
+
+
+
+
+
+
+
+// board.on("ready", function () {
+
+//   firebase.initializeApp(firebaseConfig);
+
+//   //leds
+//   const G = new five.Led(13);
+
+
+
+
+//   // vaga1
+//   firebase.database().ref('VAGA1').set({
+//     id:01,
+//     status: 'off',
+//     vagaTime: 'off',
+//     time: 1000,
+//     direction:'left'
+//   });
+
+//   // vaga2
+//   firebase.database().ref('VAGA2').set({
+//     id:02,
+//     status: 'on',
+//     vagaTime: 'off',
+//     time: 1000,
+//     direction:'right'
+
+//   });
+
+//   // vaga3
+//   firebase.database().ref('VAGA3').set({
+//     id:03,
+//     status: 'off',
+//     vagaTime: 'off',
+//     time: 1000,
+//     direction:'left'
+
+//   });
+//   // vaga4
+//   firebase.database().ref('VAGA4').set({
+//     id:04,
+//     status: 'off',
+//     vagaTime: 'off',
+//     time: 1000,
+//     direction:'right'
+
+//   });
+
+//    // vaga4
+//    firebase.database().ref('VAGA5').set({
+//     id:05,
+//     status: 'off',
+//     vagaTime: 'off',
+//     time: 1000,
+//     direction:'left'
+
+//   });
+//    // vaga4
+//    firebase.database().ref('VAGA6').set({
+//     id:06,
+//     status: 'off',
+//     vagaTime: 'off',
+//     time: 1000,
+//     direction:'right'
+
+//   });
+
+
+//    // vaga4
+//    firebase.database().ref('VAGA7').set({
+//     id:07,
+//     status: 'off',
+//     vagaTime: 'off',
+//     time: 1000,
+//     direction:'left'
+
+//   });
+//  // vaga4
+//  firebase.database().ref('VAGA8').set({
+//   id:08,
+//   status: 'off',
+//   vagaTime: 'off',
+//   time: 1000,
+//   direction:'right'
+
+// });
+
+
+
+
+
+
+
+
+//   const rootRef = firebase.database().ref().child('VAGA1');
+
+//   const vaga1Ref = rootRef.child('status');
+//   const vaga1vagaTime = rootRef.child('vagaTime');
+//   const vaga1Time = rootRef.child('time');
+
+
+
+
+
+
+
+//   let timeVaga1 = 0;
+//   vaga1Time.on('value', snap => {
+
+//     let time = snap.val()
+
+//     timeVaga1 = time
+
+
+//   });
+
+
+//   vaga1vagaTime.on('value', snap => {
+
+
+//     let temporary;
+//     if (snap.val() === "on") {
+
+
+
+//       temporary = setInterval(function () {
+//         G.blink(1000)
+//       }, timeVaga1, "sai da vaga")
+
+//     } else {
+
+//       G.off()
+
+//       clearInterval(temporary)
+
+//     }
+
+
+
+
+//   });
+
+
+
+//   vaga1Ref.on('value', snap => {
+
+
+
+//     snap.val() === "on" ? G.on()  : G.off()
+
+
+//   });
+
+
+
+//   //   vaga1 = new five.Sensor({
+//   //     pin: "A0",
+//   //     freq: 250
+//   //   });
+
+
+//   //   vaga1.on("data", function() {
+//   //     console.log(this.value)
+//   //     if(this.value < 1023 ){
+
+
+//   //       G.off()
+
+//   //       // firebase.database().ref('VAGA1').set({
+//   //       //   VAGA1:'on'
+
+//   //       // });
+
+//   //     }else if(this.value >= 1023){     
+//   //       G.on()
+//   //       // firebase.database().ref('VAGA1').set({
+//   //       //   VAGA1:'off'
+//   //       // });
+
+
+//   //     }
+
+
+//   //   });
+
+// });
+console.log("Projeto EstacionaE Iniciado!\n Integrantes:\n Alex Ferman \n Cleriston Barbosa \n Matheus Gabriel");
